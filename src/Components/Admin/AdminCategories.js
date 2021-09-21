4edbe38ed4a93716ba15970d935e85cb70cda818
samwaych <samwaych@gmail.com>
2021-09-20 20:48:08 -0400

    added POC edit modal to categories and news

--- src/Components/Admin/AdminCategories.js
+++ src/Components/Admin/AdminCategories.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { API_BASE_URL } from "../API/Api.js";
-import { Table } from "react-bootstrap";
+import { Form, Table, Button } from "react-bootstrap";
+import Modal from "../Modal/Modal.js";
 import "./Admin.css";
 
 export function AdminCategories() {
   const [categories, setCategories] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
 
   // This fetch is for the Estimates
   useEffect(() => {
@@ -47,8 +49,55 @@ export function AdminCategories() {
   }
 
   return (
-    <Table striped bordered hover size="sm" className="mx-auto w-25">
-      {categoriesTable}
-    </Table>
+    <div>
+      <Modal open={isOpen} onClose={() => setIsOpen(false)}>
+        <h5>
+          Update Services
+        </h5>
+        <Form>
+          <Form.Group>
+            <Form.Control
+              as="select"
+            >
+              <option value="accountType">Residential</option>
+              <option value="accountType">Commercial</option>
+            </Form.Control>
+          </Form.Group>
+          <Form.Group size="lg" controlId="firstName">
+            <Form.Control
+              type="text"
+              value="Service-name"
+              required
+            />
+          </Form.Group>
+          <Button
+            id="btn"
+            variant="dark"
+            block
+            size="md"
+            type="submit"
+          >
+            ADD
+          </Button>
+          <Button
+            id="btn"
+            variant="dark"
+            block
+            size="md"
+            type="submit"
+          >
+            DELETE
+          </Button>
+        </Form>
+      </Modal>
+      <Table striped bordered hover size="sm" className="mx-auto w-25">
+        {categoriesTable}
+      </Table>
+    <div className="d-flex justify-content-center">
+      <Button onClick={()=>setIsOpen(true)} variant="secondary" size="sm">
+      Edit
+      </Button>
+    </div>
+    </div>
   );
 }
