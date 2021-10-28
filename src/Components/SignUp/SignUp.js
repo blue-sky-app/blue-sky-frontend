f1142bdcc6add38e4aec532f23c4fe0d020e735c
samwaych <samwaych@gmail.com>
2021-10-28 15:25:20 -0400

    Fixed message component bugs after JWT implementation
    Message component state setter functions were not being called in the correct parts of the logic after the JWT implementation on the signup page. This is now corrected.

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -50,6 +50,10 @@ function SignUp(props) {
     props.history.push("/signUpSuccess");
   };
 
+  const successMsg = () => {
+    
+  }
+
   // Performs .post operation from array details to Db
   const sendDetailsToServer = () => {
     if (
@@ -72,23 +76,18 @@ function SignUp(props) {
         .post(API_BASE_URL + "/users/", payload)
         .then(function (response) {
           if (response.status === 200) {
-            setState((prevState) => ({
-              ...prevState,
-              display: true,
-              type: "success",
-              message: "signupSuccess",
-            }));
             redirectToSuccess();
-            setState((prevState) => ({
-              ...prevState,
-              message: null,
-            }));
-          } else {
-          }
+          } 
         })
         .catch(function (error) {
           console.log(error);
         });
+        setState((prevState) => ({
+          ...prevState,
+          display: true,
+          type: "success",
+          message: "signupSuccess",
+        }));
     } else {
       setState((prevState) => ({
         ...prevState,
@@ -111,26 +110,32 @@ function SignUp(props) {
           }));
           sendDetailsToServer();
         }
+        else {
+          setState((prevState) => ({
+            ...prevState,
+            display: true,
+            type: "fail",
+            message: "duplicate",
+          }));
+        }
+      }
+      else {
         setState((prevState) => ({
           ...prevState,
           display: true,
           type: "fail",
-          message: "duplicate",
+          message: "emailFormat",
         }));
-      }
+      } 
+    }
+    else {
       setState((prevState) => ({
         ...prevState,
         display: true,
         type: "fail",
-        message: "emailFormat",
+        message: "password",
       }));
     }
-    setState((prevState) => ({
-      ...prevState,
-      display: true,
-      type: "fail",
-      message: "password",
-    }));
   };
 
   return (
